f6e3aa8b056cb206d68d03e1737d74e55c85a297
joehua87 <joehua87@gmail.com>
2017-10-06 09:38:16 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react'
+import Layout from 'containers/Layout'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <Layout />
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/Layout/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react'
+
+function Layout() {
+  return (
+    <div>
+      Layout
+    </div>
+  )
+}
+
+export default Layout
